7e5eaabb39a5e578a834d7583eebf91f929c6770
emachado9202 <emachado9202@gmail.com>
2024-02-05 19:36:14 -0500

    refactor for use challenge

--- Program.cs
+++ Program.cs
@@ -1,13 +1,10 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using BlazingPizza.Data;
+﻿
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
@@ -25,6 +22,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
