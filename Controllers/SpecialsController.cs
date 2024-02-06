a079f7ec1723be86f2827ddd420d16b4f3afb40a
emachado9202 <emachado9202@gmail.com>
2024-02-05 22:47:24 -0500

    add controller add to program the db and controller

---
+++ Controllers/SpecialsController.cs
@@ -0,0 +1,23 @@
+﻿using BlazingPizza.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Controllers;
+
+[Route("specials")]
+[ApiController]
+public class SpecialsController
+{
+    private readonly PizzaStoreContext _context;
+
+    public SpecialsController(PizzaStoreContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<PizzaSpecial>>> GetSpecialsAsync()
+    {
+        return await _context.Specials.ToListAsync();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿
 
+using BlazingPizza.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
+builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -25,6 +30,17 @@ app.UseRouting();
 app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+using (var scope = scopeFactory.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (db.Database.EnsureCreated())
+    {
+        SeedData.Initialize(db);
+    }
+}
 
 app.Run();
 
